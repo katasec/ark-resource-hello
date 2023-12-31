43eb3773d91976a250857775babdb44e73e7babd
Ameer Deen <writeameer@gmail.com>
2023-12-31 17:27:01 +0400

    Adding test tf to thru some json

---
+++ README.md
@@ -0,0 +1,3 @@
+# Overview
+
+Spiking using Terraform resources in ark.
---
+++ main.tf
@@ -0,0 +1,23 @@
+
+
+data "local_file" "network_json" {
+  filename = "${path.module}/data.json" // Replace with your file path
+}
+
+locals {
+  network_data = jsondecode(data.local_file.network_json.content)
+}
+
+# Accessing the decoded JSON data
+output "hub_name" {
+  value = local.network_data.Hub.Name
+}
+
+output "spokes" {
+  value = [
+    for spoke in local.network_data.Spokes : {
+      Name           = spoke.Name
+      AddressPrefix  = spoke.AddressPrefix
+    }
+  ]
+}
